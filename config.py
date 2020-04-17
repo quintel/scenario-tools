738cd7618a8efefe9db58857acbb14b1f3b8f457
Chael Kruip <chaelkruip@Chaels-MacBook-Air.local>
2020-04-17 17:05:12 +0200

    Better titles

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ EXISTING_SCENARIOS = {
 
 NEW_SCENARIOS = {
     'II3050 Regionaal - Solar PV en Warmtenet fix -': {
-        'title': f"Aanpassingen gedaan op het scenario van Berenschot en Kalavasta",
+        'title': f"II3050 Regionaal - Solar PV en Warmtenet fix -",
         'area_code': 'nl',
         'short_name': 'NL_Regionaal_2050_fix',
         'end_year': '2050',
@@ -15,7 +15,7 @@ NEW_SCENARIOS = {
         'user_values': {}
     },
     'II3050 Nationaal - Solar PV en Warmtenet fix -': {
-        'title': f"Aanpassingen gedaan op het scenario van Berenschot en Kalavasta",
+        'title': f"II3050 Nationaal - Solar PV en Warmtenet fix -",
         'area_code': 'nl',
         'short_name': 'NL_Nationaal_2050_fix',
         'end_year': '2050',
@@ -23,7 +23,7 @@ NEW_SCENARIOS = {
         'user_values': {}
     },
     'II3050 Europees - Solar PV en Warmtenet fix -': {
-        'title': f"Aanpassingen gedaan op het scenario van Berenschot en Kalavasta",
+        'title': f"II3050 Europees - Solar PV en Warmtenet fix -",
         'area_code': 'nl',
         'short_name': 'NL_Europees_2050_fix',
         'end_year': '2050',
@@ -31,7 +31,7 @@ NEW_SCENARIOS = {
         'user_values': {}
     },
     'II3050 Internationaal - Solar PV en Warmtenet fix -': {
-        'title': f"Aanpassingen gedaan op het scenario van Berenschot en Kalavasta",
+        'title': f"II3050 Internationaal - Solar PV en Warmtenet fix -",
         'area_code': 'nl',
         'short_name': 'NL_Internationaal_2050_fix',
         'end_year': '2050',
