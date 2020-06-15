df1aff94fc7d44d114541be7be9e5c8e5b07ecc0
Dorine van der Vlies <dorine.vandervlies@quintel.com>
2020-06-15 16:44:28 +0200

    Updated v1 of all sectors minus industry

--- config.py
+++ config.py
@@ -1,52 +1,102 @@
 EXISTING_SCENARIOS = {
-    'regionaal': { 'id': '754910' },
-    'nationaal': { 'id': '754911' },
-    'europees': { 'id': '754912' },
-    'internationaal': { 'id': '754913' }
+    '2030_klimaatakkoord': { 'id': '764454' },
+    '2050_regionale_sturing': { 'id': '764455' },
+    '2050_nationale_sturing': { 'id': '764457' },
+    '2050_europese_sturing': { 'id': '764459' },
+    '2050_internationale_sturing': { 'id': '764460' }
 }
 
 NEW_SCENARIOS = {
-    '2030_example': {
-        'title': f"Example scenario for TenneT (2030)",
-        'area_code': 'nl',
-        'short_name': 'NL_2030_example_TenneT',
+    '2030_klimaatakkoord': {
+        'title': f"Systeemstudie Zuid-Holland 2030 v1",
+        'area_code': 'PV28_zuid_holland',
+        'short_name': 'Systeemstudie Zuid-Holland 2030 v1',
         'end_year': '2030',
-        'description': 'This is an example 2030 scenario for Tennet.',
-        'id': None,
-        'user_values': {},
-        'flexibility_order': [
-        "household_batteries",
-            "mv_batteries",
-            "electric_vehicle",
-            "opac",
-            "pumped_storage",
-            "power_to_gas",
-            "power_to_heat_industry",
-            "power_to_heat_district_heating_boiler",
-            "power_to_heat_district_heating_heatpump",
-            "power_to_kerosene",
-            "export",
-        ],
-        'heat_network_order': [
-            "energy_heat_burner_wood_pellets",
-            "energy_heat_network_storage",
-            "energy_heat_burner_waste_mix",
-            "energy_heat_heatpump_water_water_electricity",
-            "energy_heat_burner_coal",
-            "energy_heat_burner_network_gas",
-            "energy_heat_burner_crude_oil",
-            "energy_heat_burner_hydrogen",
-        ]
-    },
-    '2050_example': {
-        'title': f"Example scenario for TenneT (2050)",
-        'area_code': 'nl',
-        'short_name': 'NL_2050_example_TenneT',
-        'end_year': '2050',
-        'description': 'This is an example 2050 scenario for Tennet.',
+        'description': 'Systeemstudie Zuid-Holland 2030 v1',
         'id': None,
         'user_values': {},
         'flexibility_order': [],
         'heat_network_order': []
-    }
+    },
+    '2050_regionale_sturing': {
+        'title': f"Systeemstudie Zuid-Holland 2050 - regionale sturing v1",
+        'area_code': 'PV28_zuid_holland',
+        'short_name': '2050_regionale_sturing_v1',
+        'end_year': '2050',
+        'description': 'Systeemstudie Zuid-Holland 2050 - regionale sturing v1',
+        'id': None,
+        'user_values': {},
+        'flexibility_order': [
+            "power_to_heat_industry",
+            "electric_vehicle",
+            "power_to_gas",
+            "export"],
+        'heat_network_order': [
+            "energy_heat_heatpump_water_water_electricity",
+            "energy_heat_burner_wood_pellets",
+            "energy_heat_network_storage",
+            "energy_heat_burner_network_gas"]
+    },
+    '2050_nationale_sturing': {
+        'title': f"Systeemstudie Zuid-Holland 2030 - nationale sturing v1",
+        'area_code': 'PV28_zuid_holland',
+        'short_name': '2050_nationale_sturing_v1',
+        'end_year': '2050',
+        'description': 'Systeemstudie Zuid-Holland 2030 - nationale sturing v1',
+        'id': None,
+        'user_values': {},
+        'flexibility_order': [
+            "power_to_heat_industry",
+            "power_to_heat_district_heating_heatpump",
+            "power_to_gas",
+            "export"],
+        'heat_network_order': [
+            "energy_heat_burner_waste_mix",
+            "energy_heat_heatpump_water_water_electricity",
+            "energy_heat_burner_wood_pellets",
+            "energy_heat_network_storage",
+            "energy_heat_burner_network_gas"]
+    },
+    '2050_europese_sturing': {
+        'title': f"Systeemstudie Zuid-Holland 2030 - europese sturing v1",
+        'area_code': 'PV28_zuid_holland',
+        'short_name': '2050_europese_sturing_v1',
+        'end_year': '2050',
+        'description': 'Systeemstudie Zuid-Holland 2030 - europese sturing v1',
+        'id': None,
+        'user_values': {},
+        'flexibility_order': [
+            "power_to_heat_industry",
+            "power_to_heat_district_heating_boiler",
+            "power_to_gas",
+            "export"],
+        'heat_network_order': [
+            "energy_heat_burner_waste_mix",
+            "energy_heat_heatpump_water_water_electricity",
+            "energy_heat_burner_wood_pellets",
+            "energy_heat_network_storage",
+            "energy_heat_burner_network_gas",
+            "energy_heat_burner_hydrogen"]
+    },
+    '2050_internationale_sturing': {
+        'title': f"Systeemstudie Zuid-Holland 2030 - internationale sturing v1",
+        'area_code': 'PV28_zuid_holland',
+        'short_name': '2050_internationale_sturing_v1',
+        'end_year': '2050',
+        'description': 'Systeemstudie Zuid-Holland 2030 - internationale sturing v1',
+        'id': None,
+        'user_values': {},
+        'flexibility_order': [
+            "power_to_heat_industry",
+            "electric_vehicle",
+            "power_to_gas",
+            "export"],
+        'heat_network_order': [
+            "energy_heat_burner_waste_mix",
+            "energy_heat_heatpump_water_water_electricity",
+            "energy_heat_burner_wood_pellets",
+            "energy_heat_network_storage",
+            "energy_heat_burner_hydrogen",
+            "energy_heat_burner_network_gas"]
+    },
 }
