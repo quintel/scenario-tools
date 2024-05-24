366fc0d6aca4d339da6eed0d6838c130e5e8ad92
Kyra de Haan <kyra.dehaan@quintel.com>
2024-05-24 13:37:06 +0200

    New functions for curves and orders

--- get_template_settings.py
+++ get_template_settings.py
@@ -21,10 +21,37 @@ if __name__ == "__main__":
         template.add_user_values(API_template.get_scenario_settings())
 
         #TODO: Add getting curves
+        template.add_custom_curves(API_template.get_custom_curves())
+        print(template.custom_curves)
+        template.custom_curves.to_csv(f"data/output/{template.title}_curves.csv", index=False)
 
-        #TODO: Add getting orders: heat_network_order
-        template.add_heat_network_order(API_template.get_heat_network_order())
-        # print(template.heat_network_order)
-        
+        #TODO: Orders: combine in 1 CSV output
 
-    templates.to_csv()
+        # Heat network order
+        # heat_levels = ['ht', 'mt', 'lt']
+        # for l in heat_levels:
+        #     template.add_heat_network_orders(API_template.get_heat_network_order(subtype=l), subtype=l)
+        # df = template.get_heat_network_orders_csv(heat_levels)
+        # df.to_csv(f'data/output/heat_network_orders.csv')
+
+        # Hydrogen supply and demand order
+        # hydrogen_types = ['supply', 'demand']
+        # for t in hydrogen_types:
+        #     template.add_hydrogen_orders(API_template.get_hydrogen_orders(subtype=t), subtype=t)
+        # df = template.get_hydrogen_orders_csv(hydrogen_types)
+        # df.to_csv(f'data/output/hydrogen_orders.csv')
+
+
+        # Forecast storage order
+        # template.add_forecasting_storage_order(API_template.get_forecast_storage_order())
+        # df = template.get_forecasting_storage_order_csv()
+        # df.to_csv(f'data/output/forecasting_storage_order.csv')
+
+
+        # households_space_heating_producer_order order
+        # template.add_households_space_heating_producer_order(API_template.get_households_space_heating_producer_order())
+        # df = template.get_households_space_heating_producer_order_csv()
+        # df.to_csv(f'data/output/households_space_heating_producer_order.csv')
+
+
+    # templates.to_csv()
--- helpers/Template.py
+++ helpers/Template.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pathlib import Path
 from helpers.file_helpers import read_csv, check_duplicates, get_folder
+from helpers import curves_lst
 
 class Template:
     """
@@ -10,8 +11,17 @@ class Template:
     ATTRIBUTES = [
         'id',
         'title',
-        'user_values',
-        'heat_network_order'
+        'user_values'
+    ]
+#TODO: create one function for obtaining order csv
+    ORDERS = [
+        'heat_network_order_ht',
+        'heat_network_order_mt',
+        'heat_network_order_lt',
+        'hydrogen_supply_order',
+        'hydrogen_demand_order',
+        'forecasting_storage_order',
+        'households_space_heating_producer_order'
     ]
 
     def __init__(self, scenario_row):
@@ -29,9 +39,66 @@ class Template:
 
     def add_user_values(self, scenario_settings):
         self.user_values = scenario_settings
+    
 
-    def add_heat_network_order(self, heat_network_order):
-        self.heat_network_order = heat_network_order
+    def add_custom_curves(self, custom_curves):
+        setattr(self, 'custom_curves', custom_curves)
+
+
+    def add_heat_network_orders(self, heat_network_order, subtype):
+        setattr(self, f'heat_network_order_{subtype}', heat_network_order)
+        return heat_network_order
+    
+
+    def add_hydrogen_orders(self, hydrogen_order, subtype):
+        setattr(self, f'hydrogen_{subtype}_order', hydrogen_order)
+        return hydrogen_order
+    
+
+    def add_forecasting_storage_order(self, forecasting_storage_order):
+        setattr(self, 'forecasting_storage_order', forecasting_storage_order)
+        return forecasting_storage_order
+    
+
+    def add_households_space_heating_producer_order(self, households_space_heating_producer_order):
+        setattr(self, 'households_space_heating_producer_order', households_space_heating_producer_order)
+        return households_space_heating_producer_order
+    
+
+    def get_hydrogen_orders_csv(self, subtypes):
+        column_names = [f'hydrogen_{t}_order' for t in subtypes]
+        df = pd.DataFrame(columns = column_names)
+        for t in subtypes:
+            order_attr = f'hydrogen_{t}_order'
+            order_string = " ".join(getattr(self, order_attr)['Order'])
+            df[order_attr] = [order_string]
+        print(df)
+        return df
+    
+
+    def get_heat_network_orders_csv(self, heat_levels):
+        column_names = [f'heat_network_order_{l}' for l in heat_levels]
+        df = pd.DataFrame(columns = column_names)
+        for l in heat_levels:
+            order_attr = f'heat_network_order_{l}'
+            order_string = " ".join(getattr(self, order_attr)['Order'])
+            df[order_attr] = [order_string]
+        print(df)
+        return df
+    
+    def get_forecasting_storage_order_csv(self):
+        df = pd.DataFrame(columns = ['forecasting_storage_order'])
+        order_string = " ".join(self.forecasting_storage_order['Order'])
+        df['forecasting_storage_order'] = [order_string]
+        return df
+
+
+    def get_households_space_heating_producer_order_csv(self):
+        df = pd.DataFrame(columns = ['households_space_heating_producer_order'])
+        order_string = " ".join(self.households_space_heating_producer_order['Order'])
+        df['households_space_heating_producer_order'] = [order_string]
+        return df
+
 
 
 class TemplateCollection:
